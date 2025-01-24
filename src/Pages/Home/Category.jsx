9033b3080787e9b9dc47d8d5d7939badfc561fb0
jamil-dev <i>
2025-01-24 16:31:14 +0000

    dynamic section title added

--- src/Pages/Home/Category.jsx
+++ src/Pages/Home/Category.jsx
@@ -1,4 +1,3 @@
-
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Pagination, Navigation } from "swiper/modules";
 import "swiper/css";
@@ -9,53 +8,61 @@ import slide2 from "../../assets/home/slide2.jpg";
 import slide3 from "../../assets/home/slide3.jpg";
 import slide4 from "../../assets/home/slide4.jpg";
 import slide5 from "../../assets/home/slide5.jpg";
+import SectionTitle from "../../components/SectionTitle/SectionTitle";
 
 const Category = () => {
   return (
-    <Swiper
-      slidesPerView={3}
-      spaceBetween={10}
-      autoplay={{
-        delay: 2500,
-        disableOnInteraction: false,
-      }}
-      pagination={{
-        clickable: true,
-      }}
-      modules={[Autoplay, Pagination, Navigation]}
-      className="mySwiper my-20 max-w-4xl mx-auto cinzel-font uppercase"
-    >
-      <SwiperSlide>
-        <img src={slide1} alt="" />
-        <h3 className="text-white text-3xl font-medium text-center -mt-12 pb-15">
-          Salads
-        </h3>
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={slide2} alt="" />
-        <h3 className="text-white text-3xl font-medium text-center -mt-12 pb-15">
-          Pizzas
-        </h3>
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={slide3} alt="" />
-        <h3 className="text-white text-3xl font-medium text-center -mt-12 pb-15">
-          Soups
-        </h3>
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={slide4} alt="" />
-        <h3 className="text-white text-3xl font-medium text-center -mt-12 pb-15">
-          Desserts
-        </h3>
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={slide5} alt="" />
-        <h3 className="text-white text-3xl font-medium text-center -mt-12 pb-15">
-          Salads
-        </h3>
-      </SwiperSlide>
-    </Swiper>
+    <section className="my-10">
+      <SectionTitle
+        subHeading={"From 11:00am to 10:00pm"}
+        heading={"Book a Table"}
+        
+      ></SectionTitle>
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={10}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper mb-20 max-w-4xl mx-auto cinzel-font uppercase"
+      >
+        <SwiperSlide>
+          <img src={slide1} alt="" />
+          <h3 className="text-white text-3xl font-medium text-center -mt-12 pb-15">
+            Salads
+          </h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide2} alt="" />
+          <h3 className="text-white text-3xl font-medium text-center -mt-12 pb-15">
+            Pizzas
+          </h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide3} alt="" />
+          <h3 className="text-white text-3xl font-medium text-center -mt-12 pb-15">
+            Soups
+          </h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide4} alt="" />
+          <h3 className="text-white text-3xl font-medium text-center -mt-12 pb-15">
+            Desserts
+          </h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide5} alt="" />
+          <h3 className="text-white text-3xl font-medium text-center -mt-12 pb-15">
+            Salads
+          </h3>
+        </SwiperSlide>
+      </Swiper>
+    </section>
   );
 };
 
---
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -0,0 +1,12 @@
+const SectionTitle = ({ heading, subHeading }) => {
+  return (
+    <div className="text-center mb-10 md:w-2/12 mx-auto">
+      <p className="text-[#D99904] italic">--- {subHeading} ---</p>
+      <h3 className="text-3xl uppercase border-y-2 border-[#E8E8E8] py-2 mt-1    ">
+        {heading}
+      </h3>
+    </div>
+  );
+};
+
+export default SectionTitle;
