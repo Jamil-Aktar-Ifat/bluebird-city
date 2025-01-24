09361e7654254b3d0addeb4b6977361003a60ebb
jamil-dev <i>
2025-01-24 17:39:38 +0000

    popular menu items  section added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,15 @@
 import Banner from "./Banner";
+import Bluebird from "./BluebirdInfo";
 import Category from "./Category";
+import PopularMenu from "./PopularMenu";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
       <Category></Category>
+      <Bluebird></Bluebird>
+      <PopularMenu></PopularMenu>
     </div>
   );
 };
