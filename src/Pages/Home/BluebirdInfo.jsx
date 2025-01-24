b236b5049ad064d98513addebe31f770f180a560
jamil-dev <i>
2025-01-24 17:15:35 +0000

    bluebird info banner added

---
+++ src/Pages/Home/BluebirdInfo.jsx
@@ -0,0 +1,29 @@
+const Bluebird = () => {
+  return (
+    <div
+      className="hero my-10"
+      style={{
+        backgroundImage: "url(https://i.ibb.co/HPtLXQd/chef-service.jpg)",
+      }}
+    >
+      <div className="hero-overlay"></div>
+      <div className="hero-content text-neutral-content text-center">
+        <div className="p-10 m-20 bg-white text-black">
+          <h1 className="mb-5 text-4xl font-bold uppercase cinzel-font ">
+            bluebird city
+          </h1>
+          <p className="mb-5">
+            Bluebird City Restaurant offers a vibrant dining experience with
+            seasonal dishes, exceptional service, a curated wine list, and
+            stylish ambiance in the heart of the city, perfect for every
+            occasion. Enjoy intimate meals, relaxed atmosphere, and innovative
+            flavors designed to delight. Ideal for brunches, dinners, and
+            special celebrations.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Bluebird;
