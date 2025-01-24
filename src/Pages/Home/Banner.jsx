41fbf485022eccf033042767de8eed3b47fa3359
jamil-dev <i>
2025-01-24 15:32:09 +0000

    banner customised

--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -13,7 +13,14 @@ import img6 from "../../assets/home/06.png";
 const Banner = () => {
     
   return (
-    <Carousel className="autoplay-true">
+    <Carousel
+      autoPlay
+      infiniteLoop
+      showThumbs={false}
+      showArrows={false}
+      interval={3000}
+      
+    >
       <div>
         <img src={img1} />
       </div>
